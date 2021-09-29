c124412cce8a2ea199251309eb3c191a3cf153f1
rodrigodadam <rodrigodadam@gmail.com>
2021-09-29 16:12:11 +0000

    Add new options to container

---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+let questions = [
+    {
+
+        "question": "Web pages are designed using?",
+        "option1" : "HTML",
+        "option2": "CSS",
+        "option3": "Javascript",
+        "option4": "Jquery",
+        "option5": "PHP",
+        "answer": 2
+
+    }
+]

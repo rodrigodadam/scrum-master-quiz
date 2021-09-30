b2475bdaac9180c0055b2aee4383c2caddddfc2c
rodrigodadam <rodrigodadam@gmail.com>
2021-09-30 08:17:32 +0000

    Add event listener to options

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@ const question = document.getElementById("question");
 const options = Array.from(document.getElementsByClassName("option-text"));
 
 let currentQuestion = {};
-let acceptinsAnswers = true;
+let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let avaiableQuestions = [];
@@ -26,7 +26,8 @@ function startGame() {
 
 /**
  * Get a random question from Data,
- * Get number from data-number to access the questions atributes
+ * Get number from data-number to access the questions atributes,
+ * Splice out question used to not repeat questions
  */
 function getNewQuestion() {
     questionCounter++;
@@ -38,6 +39,27 @@ function getNewQuestion() {
         const number = option.dataset["number"];
         option.innerText = currentQuestion["option" + number];
     });
-}
+
+    avaiableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+
+};
+
+//Event listener to click in options
+
+options.forEach(option => {
+    option.addEventListener("click", i => {
+        if(!acceptingAnswers)
+            return;
+
+        acceptingAnswers = false;
+
+        const selectedOption = i.target;
+        const selectedAnswer = selectedOption.dataset["number"];
+        console.log(selectedAnswer)
+        getNewQuestion();
+    });
+});
 
 startGame();

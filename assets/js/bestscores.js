3033826777d1bdc84bb399231291d8b1c09274a0
rodrigodadam <rodrigodadam@gmail.com>
2021-10-02 11:30:02 +0000

    Return names and scores as a LI t be avaiable to see in best-scores.html as history of best scores

--- assets/js/bestscores.js
+++ assets/js/bestscores.js
@@ -1,4 +1,7 @@
 const bestScoresList = document.getElementById("bestScoresList");
 const passScores = JSON.parse(localStorage.getItem("passScores")) || [];
 
-console.log(passScores);
+bestScoresList.innerHTML = passScores.map(score => {
+    return `<li class=best-score>${score.name}-${score.score}</li>`;
+    })
+    .join("");
